3fbfab6ec77ba1c6452173642faa0c6c86864edf
Reyhan Andrea Firdaus <reihan676590@gmail.com>
2024-06-20 12:44:48 +0700

    firsh asli

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,77 @@
-import { Amiri } from "next/font/google";
-import ayat from "./lib/ayat";
+// import { Amiri } from "next/font/google";
+// const amiri = Amiri({
+//   subsets: ["arabic"],
+//   weight: ["400"]
+// })
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
 
-const amiri = Amiri({
-  subsets: ["arabic"],
-  weight: ["400"]
-})
 
 export default async function Page() {
-  let ayatk = await ayat();
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex flex-col h-screen w-full dark:from-black dark:to-slate-950 bg-gradient-to-b from-slate-50 from-60 to-cyan-50 to-40">
+      <div className="header w-full bg-gradient-to-b dark:from-sky-950 dark:to-slate-950 from-sky-800 to-slate-50 h-1/3">
+        <div className="w-full h-full">
+          <Image alt="dashboard" src={"/dashboard.svg"} className="relative left-1/2 -translate-x-1/2 w-1/2 bg-opacity-75" width={100} height={50} />
+          <div className="w-full z-50 absolute top-0 flex flex-col items-center p-4">
+            <div className="flex flex-row w-full justify-between mb-4 text-sm text-white">
+              <div>Cisayong, Tasikmalaya</div>
+              <div>
+                <div>20 Juni 2024 M</div>
+                <div>13 Dzulhijjah 1443 H</div>
+              </div>
+            </div>
+            <div className="dash-text-blur w-[90%] text-white ">
+              <i>Kata Kata takan pernah bisa mengungkapkannya rasa ini juga bla bla blaa</i>
+            </div>
+            <div className="dash-text-blur w-auto text-lg py-2 font-semibold">06 : 35 : 43</div>
+            <div className="dash-text-blur w-full shadow-md text-xs flex flex-row items-center justify-between overflow-x-scroll">
+              <div>
+                <div>Imsak</div>
+                <div>04.30 </div>
+              </div>
+              <Separator orientation="vertical" />
+              <div>
+                <div>Subuh</div>
+                <div>04.30 </div>
+              </div>
+              <Separator orientation="vertical" />
+              <div>
+                <div>Terbit</div>
+                <div>04.30 </div>
+              </div>
+              <Separator orientation="vertical" />
+              <div className="">
+                <div>Dzuhur</div>
+                <div>04.30 </div>
+              </div>
+              <Separator orientation="vertical" />
+              <div className="nextpray">
+                <div>Ashar</div>
+                <div>04.30 </div>
+              </div>
+              <Separator orientation="vertical" />
+              <div>
+                <div>Maghrib</div>
+                <div>04.30 </div>
+              </div>
+              <Separator orientation="vertical" />
+              <div>
+                <div>Isya</div>
+                <div>04.30 </div>
+              </div>
 
-      Al-Qur'an Nul Karim
-      <div className={amiri.className}>
-        {ayatk["data"][0]["text"]}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content w-full dark:bg-slate-950 rounded-t-3xl relative bg-slate-50 bg-opacity-5 backdrop-blur-sm z-20 pt-4 top-12 px-4 ">
+        <div className="grid grid-flow-col grid-cols-2 grid-rows-2 gap-3 items-center bg-opacity-50  p-4">
+          <div className="menu-dash-item dark:from-black dark:to-slate-900 bg-gradient-to-tl from-cyan-200 to-slate-50">Al-Quran</div>
+          <div className="menu-dash-item dark:from-black dark:to-slate-900 bg-gradient-to-tl from-cyan-200 to-slate-50">Hadits</div>
+          <div className="menu-dash-item dark:from-black dark:to-slate-900 bg-gradient-to-tl from-cyan-200 to-slate-50">Asmaul Husna</div>
+          <div className="menu-dash-item dark:from-black dark:to-slate-900 bg-gradient-to-tl from-cyan-200 to-slate-50">Sholat dan Doa</div>
+        </div>
       </div>
     </main>
   );
---
+++ components/pack/navbar.tsx
@@ -0,0 +1,91 @@
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar"
+import { ModeToggle } from "./themebutton"
+
+export default function Navbar() {
+  return (
+    <Menubar className="fixed bottom-0 w-full h-14 rounded-3xl bg-gradient-to-tr from-slate-50 to-cyan-200 dark:from-slate-950 dark:to-cyan-950 bg-opacity-5 backdrop-blur-md justify-between px-5">
+      <MenubarMenu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>
+            New Window <MenubarShortcut>⌘N</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem disabled>New Incognito Window</MenubarItem>
+          <MenubarSeparator />
+          <MenubarSub>
+            <MenubarSubTrigger>Share</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>Email link</MenubarItem>
+              <MenubarItem>Messages</MenubarItem>
+              <MenubarItem>Notes</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem>
+            Print... <MenubarShortcut>⌘P</MenubarShortcut>
+          </MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Edit</MenubarTrigger>
+        <MenubarContent>
+          <MenubarItem>
+            Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+          </MenubarItem>
+          <MenubarItem>
+            Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+          </MenubarItem>
+          <MenubarSeparator />
+          <MenubarSub>
+            <MenubarSubTrigger>Find</MenubarSubTrigger>
+            <MenubarSubContent>
+              <MenubarItem>Search the web</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem>Find...</MenubarItem>
+              <MenubarItem>Find Next</MenubarItem>
+              <MenubarItem>Find Previous</MenubarItem>
+            </MenubarSubContent>
+          </MenubarSub>
+          <MenubarSeparator />
+          <MenubarItem>Cut</MenubarItem>
+          <MenubarItem>Copy</MenubarItem>
+          <MenubarItem>Paste</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+      <MenubarMenu>
+        <ModeToggle />
+      </MenubarMenu>
+      <MenubarMenu>
+        <MenubarTrigger>Profiles</MenubarTrigger>
+        <MenubarContent>
+          <MenubarRadioGroup value="benoit">
+            <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
+            <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
+            <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
+          </MenubarRadioGroup>
+          <MenubarSeparator />
+          <MenubarItem inset>Edit...</MenubarItem>
+          <MenubarSeparator />
+          <MenubarItem inset>Add Profile...</MenubarItem>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
+  )
+}
