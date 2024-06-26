68cf72e9709c2fa6d233c10ea712d5a1a01cb789
Reyhan Andrea Firdaus <Reihan676590@gmail.com>
2024-06-26 07:14:46 +0000

    mobile commit dynamuc try

--- components/pack/al-qur_an/surah/detailsurah.tsx
+++ components/pack/al-qur_an/surah/detailsurah.tsx
@@ -18,7 +18,7 @@ export default function DetailSurah({ ms }: any) {
       <DialogTrigger>
         <BadgeInfo size={20} />
       </DialogTrigger>
-      <DialogContent className="rounded-lg bg-slate-200 border-emerald-500 mx-4">
+      <DialogContent className="rounded-lg bg-slate-200 border-emerald-500">
         <DialogHeader>
           <DialogTitle>{ms.nama}</DialogTitle>
           <DialogTitle>{ms.nomor} . {ms.nama_latin}</DialogTitle>
@@ -27,9 +27,9 @@ export default function DetailSurah({ ms }: any) {
             <div>Jumlah Ayat : {ms.jumlah_ayat}</div>
             <div>Diturunkan di {ms.tempat_turun}</div>
           </div>
-          <div>
-            <h3>Deskripsi</h3>
-            <div dangerouslySetInnerHTML={{ __html: ms.deskripsi }}></div>
+          <div className="flex flex-col items-start">
+            <h3 className="text-lg font-semibold" >Deskripsi</h3>
+            <div className="text-left" dangerouslySetInnerHTML={{ __html: ms.deskripsi }}></div>
           </div>
         </DialogHeader>
       </DialogContent>
