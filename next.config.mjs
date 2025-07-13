14a64b00b342e4ec37491d072638580972153943
Reyhan Andrea Firdaus <reihan676590@gmail.com>
2025-07-13 13:17:49 +0700

    update version 2.0.0

--- next.config.mjs
+++ next.config.mjs
@@ -14,4 +14,25 @@ export default withPWA({
     disable: process.env.NODE_ENV === "development",        // disable PWA in the development environment
     register: true,         // register the PWA service worker
     skipWaiting: true,      // skip waiting for service worker activation
+    runtimeCaching: [
+        {
+          urlPattern: ({ request }) => request.mode === 'navigate',
+          handler: 'NetworkFirst', // Coba jaringan dulu, jika offline baru ambil dari cache.
+          options: {
+            cacheName: 'pages',
+            expiration: {
+              maxEntries: 60,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // Cache selama 30 hari
+            },
+          },
+        },
+        {
+          // Ini aturan untuk caching file statis seperti CSS, JS, font, gambar.
+          urlPattern: /\.(?:css|js|woff2|png|jpg|jpeg|svg)$/,
+          handler: 'CacheFirst', // Ambil dari cache dulu agar loading cepat.
+          options: {
+            cacheName: 'static-assets',
+          },
+        },
+      ],
 })(nextConfig);
